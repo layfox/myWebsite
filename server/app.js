73b40d3a119245dc970d188be1041c2ac52a29c4
lianyou <layfox1@163.com>
2020-03-16 21:49:20 +0800

    修复错误

--- server/app.js
+++ server/app.js
@@ -4,9 +4,9 @@ http.createServer(function(req, res) {
     res.writeHead(200, {
         'Content-Type': 'data/json'
     });
-    res.write({
-        data: 'my first website'
-    });
+    res.write(JSON.stringify({
+        data: 'I LOVE YOU FOREVER!'
+    }));
     res.end();
 }).listen(4000);
 console.log('run server on port 4000');
